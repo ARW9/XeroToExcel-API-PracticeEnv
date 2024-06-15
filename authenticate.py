e9cf7ec0bfbc442a85628db5046bd5b314937b24
ARW9 <61503524+ARW9@users.noreply.github.com>
2024-06-15 17:17:06 -0400

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -1,5 +1,85 @@
 from flask import Flask, session, request, redirect, url_for, jsonify, render_template
 from functools import wraps
+import os
+from functools import wraps
+from io import BytesIO
+from logging.config import dictConfig
+from flask import Flask, session
+from flask_oauthlib.contrib.client import OAuth, OAuth2Application
+from flask_session import Session
+from xero_python.accounting import AccountingApi
+from xero_python.assets import AssetApi
+from xero_python.project import ProjectApi
+from xero_python.payrollau import PayrollAuApi
+from xero_python.payrolluk import PayrollUkApi
+from xero_python.payrollnz import PayrollNzApi
+from xero_python.file import FilesApi
+from xero_python.api_client import ApiClient, serialize
+from xero_python.api_client.configuration import Configuration
+from xero_python.api_client.oauth2 import OAuth2Token
+from xero_python.exceptions import AccountingBadRequestException, PayrollUkBadRequestException
+from xero_python.identity import IdentityApi
+from xero_python.utils import getvalue
+import logging_settings
+from utils import jsonify, serialize_model
+
+dictConfig(logging_settings.default_settings)
+
+# configure main flask application
+app = Flask(__name__)
+app.config.from_object("default_settings")
+app.config.from_pyfile("config.py", silent=True)
+
+if app.config["ENV"] != "production":
+    # allow oauth2 loop to run over http (used for local testing only)
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+# configure persistent session cache
+Session(app)
+
+# configure flask-oauthlib application
+oauth = OAuth(app)
+xero = oauth.remote_app(
+    name="xero",
+    version="2",
+    client_id=app.config["CLIENT_ID"],
+    client_secret=app.config["CLIENT_SECRET"],
+    endpoint_url="https://api.xero.com/",
+    authorization_url="https://login.xero.com/identity/connect/authorize",
+    access_token_url="https://identity.xero.com/connect/token",
+    refresh_token_url="https://identity.xero.com/connect/token",
+    scope="offline_access openid profile email accounting.transactions "
+    "accounting.transactions.read accounting.reports.read "
+    "accounting.journals.read accounting.settings accounting.settings.read "
+    "accounting.contacts accounting.contacts.read accounting.attachments "
+    "accounting.attachments.read assets projects "
+    "files "
+    "payroll.employees payroll.payruns payroll.payslip payroll.timesheets payroll.settings",
+)  # type: OAuth2Application
+
+
+# configure xero-python sdk client
+api_client = ApiClient(
+    Configuration(
+        debug=app.config["DEBUG"],
+        oauth2_token=OAuth2Token(
+            client_id=app.config["CLIENT_ID"], client_secret=app.config["CLIENT_SECRET"]
+        ),
+    ),
+    pool_threads=1,
+)
+
+# configure token persistence and exchange point between flask-oauthlib and xero-python
+@xero.tokengetter
+@api_client.oauth2_token_getter
+def obtain_xero_oauth2_token():
+    return session.get("token")
+
+@xero.tokensaver
+@api_client.oauth2_token_saver
+def store_xero_oauth2_token(token):
+    session["token"] = token
+    session.modified = True
 
 app = Flask(__name__)
 
